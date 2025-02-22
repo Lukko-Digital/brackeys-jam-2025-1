54fd760a91071a4ae2f1ed6fa8bb9903c9550eff
Ian Lum <ianlum314@gmail.com>
2025-02-22 12:47:23 -0500

    block movement not on walkable tiles

--- src/moveable.gd
+++ src/moveable.gd
@@ -5,7 +5,8 @@ const MOVE_LERP_SPEED = 20
 # Threshold between current position and target position that the moveable with stop moving if within
 const MOVE_DISTANCE_THRESHOLD = 2
 
-@onready var movement_ray: RayCast2D = %MovementRay
+@onready var push_ray: RayCast2D = %PushRay
+@onready var walkable_ray: RayCast2D = %WalkableRay
 
 var INPUTS = {
 	"right": Vector2.RIGHT,
@@ -31,17 +32,25 @@ func is_moving() -> bool:
 
 
 func move(dir: String) -> bool:
-	movement_ray.target_position = INPUTS[dir] * Global.TILE_SIZE
-	movement_ray.force_raycast_update()
+	push_ray.target_position = INPUTS[dir] * Global.TILE_SIZE
+	push_ray.force_raycast_update()
+	
+	if !is_walkable(dir):
+		return false
 
-	if movement_ray.is_colliding():
-		var collider = movement_ray.get_collider()
-		# If the collider isn't moveable don't move
-		if not collider is Moveable:
-			return false
-		# If the collider is moveable, and it can't move, don't move
+	if push_ray.is_colliding():
+		var collider = push_ray.get_collider()
+		# If the pushed object can't move, don't move
 		if not collider.move(dir):
 			return false
 	
 	target_position += INPUTS[dir] * Global.TILE_SIZE
 	return true
+
+func is_walkable(dir: String) -> bool:
+	var space = get_world_2d().direct_space_state
+	var query = PhysicsPointQueryParameters2D.new()
+	query.position = target_position + INPUTS[dir] * Global.TILE_SIZE
+	query.collision_mask = 1
+	var result = space.intersect_point(query)
+	return !result.is_empty()
