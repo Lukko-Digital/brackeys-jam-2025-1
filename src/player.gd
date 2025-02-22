c8cde83f002ce41c198d91661f46239b2a0bcd1f
Ian Lum <ianlum314@gmail.com>
2025-02-22 13:09:58 -0500

    add coyote timing

--- src/player.gd
+++ src/player.gd
@@ -1,9 +1,29 @@
 extends Moveable
 class_name Player
-	
+
+const COYOTE_TIME = 0.1
+
+@onready var coyote_timer: Timer = %CoyoteTimer
+
+var buffered_input: String = ""
+
+func _ready() -> void:
+	super()
+	coyote_timer.wait_time = COYOTE_TIME
+
+func movement_ended():
+	super()
+	if not coyote_timer.is_stopped():
+		move(buffered_input)
+
+func move_input(dir: String):
+	coyote_timer.start()
+	buffered_input = dir
+
+	if not is_moving:
+		move(dir)
 
 func _unhandled_input(event):
 	for dir in INPUTS.keys():
 		if event.is_action_pressed(dir):
-			if not is_moving():
-				move(dir)
+			move_input(dir)
