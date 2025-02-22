801f965d3f8d29c2279a6505730b356e36d254e6
Ian Lum <ianlum314@gmail.com>
2025-02-22 18:12:42 -0500

    level code advance line tweak coloring

--- src/level_elements/level_code.gd
+++ src/level_elements/level_code.gd
@@ -1,36 +1,51 @@
+@tool
 extends RichTextLabel
 class_name LevelCode
 
-@export_multiline var raw_code: String
-@export var red_variable: String
-@export var yellow_variable: String
-@export var blue_variable: String
-@export var teal_variable: String
+@export_multiline var raw_code: String:
+	set(str):
+		raw_code = str
+		if Engine.is_editor_hint():
+			color_code()
+		
+@export var red_variable_name: String
+@export var yellow_variable_name: String
+@export var green_variable_name: String
+@export var blue_variable_name: String
+
+const LINE_HEIGHT = 83
+const LINE_SCROLL_TIME = 0.2
 
 const VariableColors = {
 	RED = "#FF0000",
 	YELLOW = "#FFFF00",
+	GREEN = "#00FF00",
 	BLUE = "#0000FF",
-	TEAL = "#00FFFF"
 }
 
 func _ready():
 	color_code()
 
+func advance_line():
+	var scroll_bar = get_v_scroll_bar()
+	var tween: Tween = create_tween()
+	tween.tween_property(scroll_bar, "value", scroll_bar.value + LINE_HEIGHT, LINE_SCROLL_TIME)
+	
 func color_code():
-	var lines = raw_code.strip_edges().split("\n")
+	clear()
+	var lines = raw_code.split("\n")
 	for line in lines:
 		var color_line = line
 		if "//" in line:
-			color_line = "[color=#FFFFFFBB]" + line + "[/color]"
+			color_line = "[color=#000000BB]" + line + "[/color]"
 		
-		if not red_variable.is_empty() and red_variable in line:
+		elif not red_variable_name.is_empty() and red_variable_name in line:
 			color_line = "[bgcolor=" + VariableColors.RED + "]" + color_line + "[/bgcolor]"
-		if not yellow_variable.is_empty() and yellow_variable in line:
+		elif not yellow_variable_name.is_empty() and yellow_variable_name in line:
 			color_line = "[bgcolor=" + VariableColors.YELLOW + "]" + color_line + "[/bgcolor]"
-		if not blue_variable.is_empty() and blue_variable in line:
+		elif not green_variable_name.is_empty() and green_variable_name in line:
+			color_line = "[bgcolor=" + VariableColors.GREEN + "]" + color_line + "[/bgcolor]"
+		elif not blue_variable_name.is_empty() and blue_variable_name in line:
 			color_line = "[bgcolor=" + VariableColors.BLUE + "]" + color_line + "[/bgcolor]"
-		if not teal_variable.is_empty() and teal_variable in line:
-			color_line = "[bgcolor=" + VariableColors.TEAL + "]" + color_line + "[/bgcolor]"
 
 		append_text(color_line + "\n")
